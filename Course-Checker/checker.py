9daf50987fef55bbd6ab090a67524f3ea3037afd
Yaxin Cheng <yaxin.cheng@Dal.ca>
2017-06-20 09:38:17 -0400

    Updated the algorithm. Easy to modify

--- Course-Checker/checker.py
+++ Course-Checker/checker.py
@@ -4,6 +4,7 @@ import requests
 import argparse
 import webbrowser
 import sys
+__FALL__, __WINTER__, __SUMMER__ = 201810, 201820, 201830
 
 baseURL = 'https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term={term}&s_crn=&s_subj={faculty}&s_numb=&n={page}&s_district=100'
 parser = argparse.ArgumentParser(description='Search for the existence of courses')
@@ -20,10 +21,13 @@ def openURL(baseURL, term, faculty, page):
     webbrowser.open(baseURL.format(term=term, faculty=faculty, page=page))
     sys.exit(0)
 
-Page = args.page or [ (i - 1) * 20 + 1 for i in range(1, 8) ]
-termMapping = {'winter': 201820, 'fall': 201810, 'w': 201820, 'f': 201810}
-reverseTermMapping = {201810: 'Fall', 201820: 'Winter'}
-Term = [termMapping[args.term]] if not isinstance(args.term, list) else [termMapping[term] for term in args.term]
+encodePage = lambda number: int((number - 1) * 20 + 1)
+decodePage = lambda number: int((number - 1) / 20 + 1)
+
+Page = [encodePage(args.page)] if args.page else [ encodePage(i) for i in range(1, 8) ]
+reverseTermMapping = {__FALL__: 'fall', __WINTER__: 'winter', __SUMMER__: 'summer'}
+termMapping = {**{value: key for key, value in reverseTermMapping.items()}, 'w': __WINTER__, 'f': __FALL__, 's': __SUMMER__}
+Term = [termMapping[args.term.lower()]] if not isinstance(args.term, list) else [termMapping[term.lower()] for term in args.term]
 Facu = (args.faculty or 'CSCI').upper()
 if args.open:
     openURL(baseURL, Term[0], Facu, Page)
@@ -36,11 +40,10 @@ nameRegex = '<b>{faculty}\s{digit}\s.*?{name}.*?<\/b>'.format(faculty=Facu, name
 emptyRegex = '<b>{faculty}\s[0-9]*?\s.+?<\/b>'.format(faculty=Facu)
 for term in Term:
     print('Searching {name} in {term} term for {faculty}'.format(name=Facu+' '+Digit if Name == '.+?' else Name, term=reverseTermMapping[term], faculty=Facu))
-    if not isinstance(Page, list) and Page: Page = [(Page - 1) * 20 + 1]
     for index, page in enumerate(Page):
         searchURL = baseURL.format(term=term, faculty=Facu, page=page)
         source = requests.get(searchURL).text
         if not re.search(emptyRegex, source): break
-        print('Searching on page', index + 1 if len(Page) > 1 else int((page - 1)/20 + 1), ':')
+        print('Searching on page', index + 1 if len(Page) > 1 else decodePage(page), ':')
         for course in re.findall(nameRegex, source, flags=re.I):
             print(course.strip('<b></b>'))
