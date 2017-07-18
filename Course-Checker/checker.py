d2c198ee3d7c5c98f67685b1fae8e1e2cdd87fb0
Yaxin Cheng <yaxin.cheng@Dal.ca>
2017-07-18 10:47:34 -0400

    Fixed the time range bug

--- Course-Checker/checker.py
+++ Course-Checker/checker.py
@@ -1,21 +1,21 @@
 #!/Users/Yaxin.Cheng@iCloud.com/Developer/venv/bin/python
 import re, argparse, webbrowser, sys
 import requests
-from datetime import datetime
+from datetime import datetime 
 
 baseURL = 'https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term={term}&s_crn=&s_subj={faculty}&s_numb=&n={page}&s_district=All'
 parser = argparse.ArgumentParser(description='Search for the existence of courses')
 parser.add_argument('-o', '--open', action='store_true', help='Open the url in the browser')
 parser.add_argument('-s', '--show', action='store_true', help='Show course dates')
 parser.add_argument('-t', '--term', nargs='*', default=['f', 'w'], help='Term (Available input: winter/fall/w/f; fall and winter by default)')
-parser.add_argument('-d', '--digit', nargs='*', help='Course Digit (Fuzzy search available (regex); Cannot search with name at the same time)')
+parser.add_argument('-d', '--digit', nargs='*', help='Course Digit (Fuzzy search available (regex))')
 parser.add_argument('-n', '--name', nargs='*', help='Name (Partial search is available; Cannot search with digit at the same time)')
 parser.add_argument('-f', '--faculty', help='Faculty (Shorthand of faculty name)')
 parser.add_argument('-r', '--range', metavar='TIME', default='0:2400', help='''Time range the courses are available. Example: ":1605"=before 16:05, "1605:"=after 16:05, "1505:1605"=between 15:05 and 16:05''')
 parser.add_argument('-w', '--week', default='MTRWF', help='Week days which courses are available; Example: MWF: Mon, Wed, Fri')
 parser.add_argument('-y', '--year', type=int, help='Year of the timetable (Historical data may be unaccessible)')
 args = parser.parse_args() 
-if not (args.faculty and (args.name or args.digit)): 
+if not (args.faculty and (args.name or args.digit or args.range)): 
     parser.print_help()
     sys.exit(0)
 
@@ -37,11 +37,12 @@ termMapping = {**{value: key for key, value in reverseTermMapping.items()}, 'w':
 Term = [termMapping[args.term.lower()]] if not isinstance(args.term, list) else [termMapping[term.lower()] for term in args.term]
 Facu = args.faculty.upper()
 Week = args.week
-Time = list(map(encodeTime, args.range.split(':')))
-if args.open:
-    openURL(baseURL, Term[0], Facu, Page)
-Name = '.+?' if args.digit else '(' + '|'.join(args.name) + ')'
-Digit = '(' + '|'.join(args.digit) + ')' if args.digit else '\d*?'
+if args.range.startswith(':'): Time = list(map(encodeTime, ('0'+args.range).split(':')))
+elif args.range.endswith(':'): Time = list(map(encodeTime, (args.range+'2400').split(':')))
+else: Time = list(map(encodeTime, (args.range+'0').split(':')))
+if args.open: openURL(baseURL, Term[0], Facu, Page)
+Name = '({})'.format('|'.join(args.name)) if args.name else '.+?'
+Digit = '({})'.format('|'.join(args.digit)) if args.digit else '\d*?'
 
 crnRegex  = re.compile('<b>\d{5}<\/b>')
 coreRegex = re.compile('^<TD.*?COLSPAN="15" CLASS="detthdr">(.|\s)*?<tr.*valign=', re.M)
@@ -55,8 +56,12 @@ emptyRegex = re.compile('<b>{faculty}\s[0-9]*?\s.+?<\/b>'.format(faculty=Facu))
 clearRegex = re.compile('(<b>)|(<\/b>)')
 splitRegex = re.compile('<\/tr>\s*?<tr>')
 
+if Name == '.+?' and Digit == '\d*?': searchingName = '{faculty} between {From} and {End}'.format(faculty=Facu, From=Time[0], End=Time[1])
+elif Name != '.+?': searchingName = '{name} between {From} and {End}'.format(name=Name, From=Time[0], End=Time[1])
+else: searchingName = '{faculty} {digit} between {From} and {End}'.format(faculty=Facu, digit=Digit, From=Time[0], End=Time[1])
+
 for term in Term:
-    print('Searching {name} in {term} term for {faculty} (Year {year})\n'.format(name=Facu+' '+str(Digit) if Name == '.+?' else Name, term=reverseTermMapping[term], faculty=Facu, year=decodeYear(year)))
+    print('Searching {name} in {term} term for {faculty} (Year {year})\n'.format(name=searchingName, term=reverseTermMapping[term], faculty=Facu, year=decodeYear(year)))
     print('=' * 50)
     for index, page in enumerate(Page):
         searchURL = baseURL.format(term=term, faculty=Facu, page=page)
