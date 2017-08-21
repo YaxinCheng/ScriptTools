ab66d2c38467864a980c75dd7359aaab5f420734
Yaxin Cheng <yaxin.cheng@Dal.ca>
2017-08-21 10:11:57 -0400

    Enabled search for no schedule courses
    Courses like COOP and Directed Study have no time schedule, now they are searchable through this script;
    Dates are shown automatically for summer courses.

--- Course-Checker/checker.py
+++ Course-Checker/checker.py
@@ -6,7 +6,6 @@ from datetime import datetime
 baseURL = 'https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term={term}&s_crn=&s_subj={faculty}&s_numb=&n={page}&s_district=All'
 parser = argparse.ArgumentParser(description='Search for the existence of courses')
 parser.add_argument('-o', '--open', action='store_true', help='Open the url in the browser')
-parser.add_argument('-s', '--show', action='store_true', help='Show course dates')
 parser.add_argument('-t', '--term', nargs='*', default=['f', 'w'], help='Term (Available input: winter/fall/summer/w/f/s; fall and winter by default)')
 parser.add_argument('-d', '--digit', nargs='*', help='Course Digit (Fuzzy search available (regex))')
 parser.add_argument('-n', '--name', nargs='*', help='Name (Partial search is available)')
@@ -27,7 +26,6 @@ encodePage = lambda number: int((number - 1) * 20 + 1)
 encodeYear = lambda year: (year + 1) * 100
 decodeYear = lambda year: int(year / 100) - 1
 encodeTime = lambda time: int(time)
-
 year = encodeYear(args.year or datetime.now().year)
 __FALL__, __WINTER__, __SUMMER__ = year + 10, year + 20, year + 30
 
@@ -55,7 +53,6 @@ percRegex = re.compile('(\d{1,2}\.\d{1,2}\%)|(WLIST)|(FULL)')
 emptyRegex = re.compile('<b>{faculty}\s[0-9]*?\s.+?<\/b>'.format(faculty=Facu))
 clearRegex = re.compile('(<b>)|(<\/b>)')
 splitRegex = re.compile('<\/tr>\s*?<tr>')
-
 if Name == '.+?' and Digit == '\d*?': searchingName = '{faculty} between {From} and {End}'.format(faculty=Facu, From=Time[0], End=Time[1])
 elif Name != '.+?': searchingName = '{name} between {From} and {End}'.format(name=Name, From=Time[0], End=Time[1])
 else: searchingName = '{faculty} {digit} between {From} and {End}'.format(faculty=Facu, digit=Digit, From=Time[0], End=Time[1])
@@ -75,7 +72,7 @@ try:
                     name = nameRegex.search(components[0]).group()
                     header = clearRegex.sub('', name) + '\n'
                 except: continue
-                if args.show: header += dateRegex.search(components[0]).group() + '\n'
+                if not (term - 30) % 100: header += 'Date: ' + dateRegex.search(components[0]).group() + '\n' # Show dates only in summers
                 content = ''
                 for detail in components[1:]:
                     try: crn = crnRegex.search(detail).group().strip('<b></b>')
@@ -83,14 +80,17 @@ try:
                     ctype = typeRegex.search(detail).group()
                     if ctype == 'Lec':
                         weeks = '|'.join(map(lambda week: week.strip('<p class="centeraligntext"></p>'), weekRegex.findall(detail)))
-                        printable = len(weeks)
+                        printable = len(weeks) or Week != 'MTWRF'
                         if not printable: break
                     else: weeks = '|'.join(map(lambda week: week.strip('<p class="centeraligntext"></p>'), re.findall('<p class="centeraligntext">[MTWRF]<\/p>', detail))) 
                     try: time = timeRegex.search(detail).group()
                     except: time = 'N.A.\t'
                     if ctype == 'Lec': 
-                        pre, post = [int(each) for each in time.split('-')]
-                        printable = Time[0] <= pre and post <= Time[1]
+                        if time == 'N.A.\t':
+                            printable = Time == [0, 2400] and Week == 'MTWRF'
+                        else:
+                            pre, post = [int(each) for each in time.split('-')]
+                            printable = Time[0] <= pre and post <= Time[1]
                         if not printable: break
                     percent = percRegex.search(detail).group()
                     content += '\t'.join([crn, ctype, weeks, time, percent]) + '\n'
